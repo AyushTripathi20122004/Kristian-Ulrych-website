68dcb7db28e72360814404531b00ef5183ba6355
Aayush Tripaathi <ayushsharma48702@gmail.com>
2026-06-23 13:08:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,8 +23,7 @@ const imageLink=
     "https://www.ulrychkristian.cz/_next/image?url=%2Fimages%2Fveevoy%2FMacBook%20Pro%2016.png&w=3840&q=75"
     ,
     "https://www.ulrychkristian.cz/_next/image?url=%2Fimages%2Fmapwhizz%2Fmapwhizz-commute-analysis-macbook.png&w=3840&q=75"
-    ,
-    "https://www.ulrychkristian.cz/_next/image?url=%2Fimages%2Fhero_portrait.jpeg&w=640&q=75"
+    
     ]
 
 const autoImage=document.querySelector('.image');
@@ -45,3 +44,16 @@ setInterval(()=>{
     changeImage()
     index=(index+1)%(imageLink.length)
 },2500)
+
+
+// get in touch button page start here
+
+const GetInTouchbtn=document.querySelector('.GetInTouchbtn');
+const FormWindow=document.querySelector('.GetInTouchSection')
+const MainSection=document.querySelector('.MainSection');
+GetInTouchbtn.addEventListener('click',()=>{
+    console.log('btn clicked');
+    
+    MainSection.style.display='none';
+    FormWindow.style.display='flex';
+})
